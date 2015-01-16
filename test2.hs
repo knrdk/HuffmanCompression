1add2d0b7846bf46bf6b1db154b499d1848b46c4
knrdk <konrad1991@gmail.com>
2015-01-16 22:18:22 +0100

    zapisywanie tablicy bitów do pliku!

--- test2.hs
+++ test2.hs
@@ -1,11 +1,10 @@
-import Data.ByteString.Lazy as BS (readFile, unpack)
+import Data.ByteString as BS (readFile, writeFile, unpack, pack)
 import Data.Word
 import Data.Bits
-import Data.Binary
 
 main = do	
-	b <- getBits "foo.txt"
-	print $ take 20 b
+	b <- getBits "foo.txt"	
+	BS.writeFile "copy.txt" $ bitArrayToByteString b
 
 data Bit = Zero | One
 
@@ -17,11 +16,6 @@ getBit :: Bool -> Bit
 getBit False = Zero
 getBit True = One
 	
-{-
-fileToByteString :: String -> IO [ByteString]
-fileToByteString fp = do
-    return BS.readFile    
-	-}
 getBits :: String -> IO [Bit]
 getBits fp = do
 	contents <- fileToWordList fp
@@ -31,12 +25,30 @@ fileToWordList :: String -> IO [Word8]
 fileToWordList fp = do
     contents <- BS.readFile fp
     return $ unpack contents
-
+	
 word8ToByteArray x = word8ToByteArray' 7 x
 	
 --Word8ToByteArray' :: Int -> Word8 -> [Bool]
 word8ToByteArray' (-1) x = []
 word8ToByteArray' i y = (getBit $ testBit y i) : (word8ToByteArray' (i-1) y)
 
-test 3 x = 0
-test n x = 2*n
+---
+zeroes = Zero : zeroes
+
+
+bitArrayToByteString = BS.pack . bitArrayToWord8Array
+
+bitArrayToWord8Array x = map byteToWord8 $ chop 8 x
+
+chop _ [] = []
+chop n xs = take n xs : chop n (drop n xs)
+
+byteToWord8 x = (byteToInt x) :: Word8
+
+bitArrayToByte x = take 8 (x++zeroes)
+
+byteToInt = reversedByteToInt . reverse . bitArrayToByte
+
+reversedByteToInt [] = 0
+reversedByteToInt (One:xs) = 1 + 2 * (reversedByteToInt xs)
+reversedByteToInt (Zero:xs) = 2 * (reversedByteToInt xs)
