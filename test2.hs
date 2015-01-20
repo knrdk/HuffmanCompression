ba944865bbc088c71c24e4f5f492a71246ba857f
knrdk <konrad1991@gmail.com>
2015-01-20 19:10:14 +0100

    tworzenie drzewa Huffmana

--- test2.hs
+++ test2.hs
@@ -2,31 +2,20 @@ import Data.ByteString.Lazy as BS (readFile, writeFile, unpack, pack)
 import Data.Word
 import Data.Bits
 
-main = do	
+copy = do	
 	b <- fileToBitArray "foo.txt"
-	BS.writeFile "bar.txt" $ bitArrayToByteString $ map neg b
+	BS.writeFile "bar.txt" $ bitArrayToByteString b		
 
 ---
 	
 data Bit = Zero | One
 
-{-
-instance Show Bit where
-	show (Zero) = "0"
-	show (One) = "1"
--}
 getBit :: Bool -> Bit
 getBit False = Zero
 getBit True = One
 
 zeroes = Zero : zeroes
-	
------
-
-neg :: Bit -> Bit
-neg One = Zero
-neg Zero = One	
-	
+		
 -----
 	
 fileToBitArray :: String -> IO [Bit]
@@ -67,3 +56,54 @@ byteToWord8' n (Zero:xs) = byteToWord8' (n-1) xs
 
 bitArrayToByte :: [Bit] -> [Bit]
 bitArrayToByte x = take 8 (x++zeroes)
+
+-----
+
+getHuffmanTrees :: [Word8] -> [HuffmanTree Word8]
+getHuffmanTrees = (map getHuffmanTree) . getFrequencies 
+
+getHuffmanTree :: (Word8, Int) -> HuffmanTree Word8
+getHuffmanTree (x,weight) = Leaf x weight
+
+getFrequencies :: (Eq a, Ord a) => [a] -> [(a,Int)]
+getFrequencies = getFrequencies' . sort
+
+getFrequencies' :: (Eq a) => [a] -> [(a,Int)]
+getFrequencies' [] = []
+getFrequencies' (w:ws) = getFrequencies'' ws (w,1)
+
+getFrequencies'' :: (Eq a) => [a] -> (a,Int) -> [(a,Int)]
+getFrequencies'' [] current = [current]
+getFrequencies'' (w:ws) current
+	| w == fst current = getFrequencies'' ws (w, ((snd current)+1))
+	| otherwise = [current] ++ (getFrequencies'' ws (w,1))
+
+
+sort :: (Ord a)=>[a]->[a]
+sort [] = []
+sort (x:xs) = (sort [y|y<-xs,y<x]) ++ [x] ++ [y|y<-xs,y==x] ++ (sort [y|y<-xs,y>x])	
+
+-----
+
+data HuffmanTree a = Leaf a Int | Node (HuffmanTree a) (HuffmanTree a) Int deriving (Show)
+
+instance Eq (HuffmanTree a) where
+	x == y = (weight x) == (weight y)
+
+instance Ord (HuffmanTree a) where
+	x `compare` y = (weight x) `compare` (weight y)
+
+weight :: HuffmanTree a -> Int
+weight (Leaf _ w) = w
+weight (Node _ _ w) = w
+
+merge :: HuffmanTree a -> HuffmanTree a -> HuffmanTree a
+merge x y = Node x y (weight x + weight y)
+
+buildTree :: [HuffmanTree a] -> HuffmanTree a
+buildTree = buildTree' . sort
+
+buildTree' :: [HuffmanTree a] -> HuffmanTree a
+buildTree' (x:[]) = x
+buildTree' (x:y:[]) = merge x y
+buildTree' (x:y:xs) = buildTree ((merge x y):xs) 
